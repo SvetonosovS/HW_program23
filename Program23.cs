30f5e72b06a8161f5fc6edea7cc20ae97c5626ea
SvetonosovS <tris23@bk.ru>
2022-10-09 23:55:23 +0300

    prog23

---
+++ Program23.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+PrintSquares(N);
+
+
+static void PrintSquares(int N)
+{
+    for(int i = 1; i < N; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+    Console.Write($"{N*N*N}");
+}
